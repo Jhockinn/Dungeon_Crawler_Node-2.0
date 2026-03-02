586c85a57974242fcbb31e612b084051e7c0e619
Tobias Rune Lagermann Nielsen <tobiln@hotmail.dk>
2026-03-02 09:07:12 +0100

    final commit

--- server/app.js
+++ server/app.js
@@ -58,7 +58,7 @@ io.use((socket, next) => {
 
 setupSockets(io);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
